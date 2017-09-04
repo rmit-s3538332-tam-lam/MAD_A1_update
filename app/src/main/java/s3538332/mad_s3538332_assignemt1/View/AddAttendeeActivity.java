77730c8ecdf0e0dd937caa1c1d65fe761202551f
Tam Lam <Tam@11302934523M.local>
2017-09-05 02:57:38 +1000

    getresult

--- app/src/main/java/s3538332/mad_s3538332_assignemt1/View/AddAttendeeActivity.java
+++ app/src/main/java/s3538332/mad_s3538332_assignemt1/View/AddAttendeeActivity.java
@@ -1,9 +1,14 @@
 package s3538332.mad_s3538332_assignemt1.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,12 +26,27 @@ public class AddAttendeeActivity extends AppCompatActivity {
         friendListView = (ListView) findViewById(R.id.friendListView);
         controller = new Controller(this);
         populateListView();
+        setListItemListener();
     }
+
     public void populateListView() {
         ArrayList<String> nameOnlyList = controller.nameOnlyList();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.itemlistview,
                 nameOnlyList);
         friendListView.setAdapter(adapter);
 
+    }
+    public void setListItemListener(){
+        friendListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent();
+                intent.putExtra("ID",i);
+                Log.i(controller.LOG_TAG, "ID: " + i);
+                setResult(RESULT_OK,intent);
+                finish();
+
+            }
+        });
     }
 }
--- app/src/main/java/s3538332/mad_s3538332_assignemt1/View/ViewAnttendeeActivity.java
+++ app/src/main/java/s3538332/mad_s3538332_assignemt1/View/ViewAnttendeeActivity.java
@@ -1,23 +1,47 @@
 package s3538332.mad_s3538332_assignemt1.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import s3538332.mad_s3538332_assignemt1.Controller.Controller;
 import s3538332.mad_s3538332_assignemt1.Controller.popActListener;
 import s3538332.mad_s3538332_assignemt1.R;
 
 public class ViewAnttendeeActivity extends AppCompatActivity {
     Button addAttendeeBtn;
     ListView attendeeListView;
+    static int REQ_CODE = 1;
+    Controller controller;
+    int id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_anttendee);
         addAttendeeBtn = (Button) findViewById(R.id.addAttendeeBtn);
         attendeeListView = (ListView) findViewById(R.id.attendeeListView);
-
+        controller = new Controller(this);
         addAttendeeBtn.setOnClickListener(new popActListener(this, AddAttendeeActivity.class));
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, AddAttendeeActivity.class);
+        startActivityForResult(intent, REQ_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQ_CODE && resultCode == RESULT_OK && data != null) {
+            id = data.getIntExtra("ID", 0);
+//            Log.i(controller.LOG_TAG, "ID " + id);
+            Toast.makeText(this, "ID: " + id, Toast.LENGTH_SHORT).show();
+        }
     }
 }
