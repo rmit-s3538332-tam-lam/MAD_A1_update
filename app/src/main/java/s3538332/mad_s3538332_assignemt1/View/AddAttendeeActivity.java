aa677f910d86b1d7cb2b0f717b010f8da808009f
Tam Lam <Tam@11302934523M.local>
2017-09-05 02:39:20 +1000

    populate AddAttendeeActivity

--- app/src/main/java/s3538332/mad_s3538332_assignemt1/View/AddAttendeeActivity.java
+++ app/src/main/java/s3538332/mad_s3538332_assignemt1/View/AddAttendeeActivity.java
@@ -2,14 +2,31 @@ package s3538332.mad_s3538332_assignemt1.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import s3538332.mad_s3538332_assignemt1.Controller.Controller;
 import s3538332.mad_s3538332_assignemt1.R;
 
 public class AddAttendeeActivity extends AppCompatActivity {
+    ListView friendListView;
+    Controller controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_attendee);
+        friendListView = (ListView) findViewById(R.id.friendListView);
+        controller = new Controller(this);
+        populateListView();
+    }
+    public void populateListView() {
+        ArrayList<String> nameOnlyList = controller.nameOnlyList();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.itemlistview,
+                nameOnlyList);
+        friendListView.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/s3538332/mad_s3538332_assignemt1/View/AddFriendActivity.java
+++ app/src/main/java/s3538332/mad_s3538332_assignemt1/View/AddFriendActivity.java
@@ -2,14 +2,19 @@ package s3538332.mad_s3538332_assignemt1.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 import s3538332.mad_s3538332_assignemt1.R;
 
 public class AddFriendActivity extends AppCompatActivity {
-
+    ListView friendListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+
+        friendListView = (ListView) findViewById(R.id.friendListView);
     }
 }
