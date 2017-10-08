e5bb0859e5c2dfe73cfcbc158e8b4a6eafbdddb8
Tam Lam <tamlam@Tams-MacBook.local>
2017-10-08 13:36:37 +1100

    add Your location view

--- app/src/main/java/s3538332/mad_s3538332_assignemt1/View/MainActivity.java
+++ app/src/main/java/s3538332/mad_s3538332_assignemt1/View/MainActivity.java
@@ -10,7 +10,7 @@ import s3538332.mad_s3538332_assignemt1.Controller.popActListener;
 import s3538332.mad_s3538332_assignemt1.R;
 
 public class MainActivity extends AppCompatActivity {
-    Button friendsBtn, meetingsBtn;
+    Button friendsBtn, meetingsBtn,yourLocationBtn;
     MeetingDBController meetingDBController;
     FriendDBController friendDBController;
 
@@ -21,13 +21,17 @@ public class MainActivity extends AppCompatActivity {
         meetingDBController = new MeetingDBController(this);
         friendDBController = new FriendDBController(this);
 
+        yourLocationBtn = (Button) findViewById(R.id.yourLocationBtn);
         friendsBtn = (Button) findViewById(R.id.friendsBtn);
         meetingsBtn = (Button) findViewById(R.id.meetingsBtn);
 
         popActListener friendsBtnListener = new popActListener(this, FriendListActivity.class);
         popActListener meetingsBtnListener = new popActListener(this,MeetingListActivity.class);
+        popActListener yourLocationBtnListener = new popActListener(this, YourLocationActivity.class);
         friendsBtn.setOnClickListener(friendsBtnListener);
         meetingsBtn.setOnClickListener(meetingsBtnListener);
+        yourLocationBtn.setOnClickListener(yourLocationBtnListener);
+
 
     }
 
